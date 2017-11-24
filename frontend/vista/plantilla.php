a43b19d1ad11d385f9fd176531484a156c22414a
rositadiazgalaz <rositadiazgalaz@gmail.com>
2017-11-24 10:56:15 -0300

    maquetacion frontend

---
+++ frontend/vista/plantilla.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width-device-width, initial-scale-1.0, minimum-ascale-1.0, maximum-scale-1.0. user-scalable=no">
+	<meta name="title" content="Tienda Virtual Cosmética Savia">
+	<meta name="description" content="Cosmética Savia se dedica a producir y entregar productos salidos directamente de la naturaleza, libres de crueldad animal. ven a conocer nuestra linea de cremas corporales y cara además de nuestros productos capilares.">
+	<meta name="keyword" content="cosmetica natural cremas naturales libres de conservantes crema de cuerpo sin preservantes crema de cara sin preservantes shampoo natural humectante de labios natural ">
+	<title>Cosmetica Savia</title>
+</head>
+<body>
+	<h1>Titulo</h1>
+
+</body>
+</html>
