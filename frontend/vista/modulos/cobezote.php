c99b1559bd03447cb11920600905bbd6bd0995d6
rositadiazgalaz <rositadiazgalaz@gmail.com>
2017-11-28 13:16:21 -0300

    agregando estilos header

---
+++ frontend/vista/modulos/cobezote.php
@@ -0,0 +1,254 @@
+<!--=========================
+=            TOP            =
+==========================-->
+
+<div class="container-fluid barraSuperior" id="top">
+	
+	<div class="container">
+		
+		<div class="row">
+			<!--============================
+			=            SOCIAL            =
+			=============================-->
+			<div class="col-lg-9 col-md-9 col-sm-8 col-xs-12 social">
+				<ul>
+					<li>
+						<a href="http://facebook.com/" target="_blank">
+							<i class="fa fa-facebook redSocial facebookBlanco" aria-hidden="true"></i>
+							
+						</a>
+						
+					</li>
+
+					<li>
+						<a href="http://youtube.com/" target="_blank">
+							<i class="fa fa-youtube redSocial youtubeBlanco" aria-hidden=true></i>
+						</a>
+					</li>
+
+					<li>
+						<a href="http//twitter.com/" target="_blank">
+							<i class="fa fa-twitter redSocial twitterBlanco" aria-hidden="true"></i> 
+						</a>
+					</li>
+
+					<li>
+						<a href="http//google.com/" target="_blank">
+							<i class="fa fa-google-plus redSocial googleBlanco" aria-hidden="true"></i>
+						</a>
+
+					</li>
+
+					<li>
+						<a href="http://instagram.com/" target="_blank">
+							<i class="fa fa-instagram redSocial instagramBlanco" aria-hidden="true"></i>
+						</a>
+
+					</li>
+
+				</ul>
+
+			</div>
+			
+			<!--====  End of SOCIAL  ====-->
+			
+			<!--==============================
+			=            REGISTRO            =
+			===============================-->
+			<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 registro">
+				
+				<ul>
+					<li><a href="#modalIngreso" data-toggle="modal">Ingresar</a></li>
+					<li>|</li>
+					<li><a href="#modalRegistro" data-toggle="modal">Crear una cuenta</a></li>
+
+				</ul>
+
+			</div>
+			<!--====  End of REGISTRO  ====-->
+
+		</div>
+
+	</div>
+
+</div>
+<!--====  End of TOP  ====-->
+
+<!--============================
+=            HEADER            =
+=============================-->
+<header class="container-fluid">
+
+	<div class="container">
+
+		<div class="row" id="cabezote">
+			<!--==============================
+			=            LOGOTIPO            =
+			===============================-->
+			<div class="col-lg-3 col-md-3 col-sm-2 col-xs-12" id="logotipo">
+				<a href="#">
+					<img src="http://localhost:8080/saviacosmetica/backend/vistas/img/plantilla/logo.png" alt="Cosmetica savia natural desde dentro" class="img-responsive">
+				</a>
+			</div>	
+			<!--====  End of LOGOTIPO  ====-->
+			<!--==================================================
+			=            BLOQUE CATEGORIAS Y BUSCADOR            =
+			===================================================-->
+			<div class="col-lg-6 col-md-6 col-sm-8 col-xs-12">
+				<!--======================================
+				=            BOTÓN CATEGORÍAS            =
+				=======================================-->
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 backColor" id="btnCategorias">
+
+					<p>CATEGORÍAS
+
+						<span class="pull-right">
+							<i class="fa fa-bars" aria-hidden="true"></i>
+						</span>
+
+					</p>
+
+				</div>
+				<!--====  End of BOTÓN CATEGORÍAS  ====-->
+
+				<!--==============================
+				=            BUSCADOR            =
+				===============================-->
+				<div class="input-group col-lg-8 col-md-8 col-sm-8 col-xs-12" id=buscador>
+
+					<input type="search" name="buscar" class="form-control" placeholder="Buscar...">
+
+					<span class="input-group-btn">
+
+						<a href="#">
+
+							<button class="btn btn-default backColor" type="submit">
+
+								<i class="fa fa-search"></i>
+
+							</button>
+						</a>
+
+					</span>
+
+				</div>
+				<!--====  End of BUSCADOR  ====-->	
+
+			</div>
+			<!--====  End of BLOQUE CATEGORIAS Y BUSCADOR  ====-->
+
+			<!--========================================
+			=            CARRITO DE COMPRAS            =
+			=========================================-->
+			<div class="col-lg-3 col-md-3 col-sm-2 col-xs-12" id="carrito">
+
+				<a href="#">
+					
+					<button class="btn btn-default pull-left backColor">
+						
+						<i class="fa fa-shopping-cart" aria-hidden="true"></i>
+
+					</button>
+
+				</a>
+				<p>TU CESTA<span class="cantidadCesta"></span>
+					<br>USD $<span class="sumaCesta"></span>
+				</p>
+				
+
+			</div>
+			<!--====  End of CARRITO DE COMPRAS  ====-->
+			
+		</div>
+		
+		<!--================================
+		=            CATEGORÍAS            =
+		=================================-->
+		
+		<div class="col-xs-12 backColor" id="categorias" >
+			<!--INICIO  PRIMERA  CATEGORIA  -->
+			<div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+
+				<h4>
+					<a href="#" class="pixelCategorias">Nombre Categoría</a>
+				</h4> 
+
+				<hr> <!-- LINEA DIVISION -->
+
+				<ul>
+					<li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+					<li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+					<li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+					<li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+					<li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+				</ul>
+
+			</div> <!--FIN  PRIMERA  CATEGORIA  -->
+
+			<!--INICIO SEGUNDA  CATEGORIA  -->
+			<div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+
+				<h4>
+					<a href="#" class="pixelCategorias">Nombre Categoría</a>
+				</h4> 
+
+				<hr> <!-- LINEA DIVISION -->
+
+				<ul>
+					<li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+					<li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+					<li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+					<li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+					<li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+				</ul>
+
+			</div>  <!-- FIN SEGUNDA CATEGORIA  -->
+
+			<!--INICIO TERCERA  CATEGORIA  -->
+			<div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+
+				<h4>
+					<a href="#" class="pixelCategorias">Nombre Categoría</a>
+				</h4> 
+
+				<hr> <!-- LINEA DIVISION -->
+
+				<ul>
+					<li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+					<li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+					<li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+					<li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+					<li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+				</ul>
+
+			</div> <!--FIN TERCERA  CATEGORIA  -->
+
+			<!--INICIO CUARTA  CATEGORIA  -->
+			<div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+
+				<h4>
+					<a href="#" class="pixelCategorias">Nombre Categoría</a>
+				</h4> 
+
+				<hr> <!-- LINEA DIVISION -->
+
+				<ul>
+					<li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+					<li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+					<li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+					<li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+					<li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+				</ul>
+
+			</div> <!--FIN CUARTA CATEGORIA  -->
+
+			
+		</div>
+		<!--====  End of CATEGORÍAS  ====-->
+		
+	</div>
+	
+
+</header>
+<!--====  End of HEADER  ====-->
+
--- frontend/vista/plantilla.php
+++ frontend/vista/plantilla.php
@@ -2,7 +2,7 @@
 <html lang="es">
 <head>
 	<meta charset="UTF-8">
-	<meta name="viewport" content="width-device-width, initial-scale-1.0, minimum-ascale-1.0, maximum-scale-1.0. user-scalable=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
 	<meta name="title" content="Tienda Virtual Cosmética Savia">
 
@@ -11,10 +11,43 @@
 	<meta name="keyword" content="cosmetica natural cremas naturales libres de conservantes crema de cuerpo sin preservantes crema de cara sin preservantes shampoo natural humectante de labios natural ">
 
 	<title>Cosmetica Savia</title>
+	
+
+	<link rel="stylesheet" href="vista/css/plugins/bootstrap.min.css">
+
+	<link rel="stylesheet" href="vista/css/plugins/font-awesome.min.css">
+
+	<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+
+	<link href="https://fonts.googleapis.com/css?family=Ubuntu+Condensed" rel="stylesheet">	
+
+	<link rel="stylesheet" href="vista/css/plantilla.css">
+
+	<link rel="stylesheet" href="vista/css/cabezote.css">
+	
+	<script src="vista/js/plugins/jquery.min.js"></script>
+
+	<script src="vista/js/plugins/bootstrap.min.js"></script>
+
 </head>
 
 <body>
-	<h1>Titulo</h1>
+
+<?php
+
+/*=============================================
+=            CABEZOTE  php          =
+=============================================*/
+
+include "modulos/cobezote.php";
+
+
+?>
+
+
+
+	
+
 
 </body>
 </html>
